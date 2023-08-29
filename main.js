cb3b837ad93c12a2b3a0c6d03a4255cbbd107cb2
olive uzoma <olive.c.uzoma@gmail.com>
2023-08-29 06:33:26 +0100

    alert admin name aumal

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 "use strict";
 
 // this code works the modern way
-alert('Hello'); alert('World');
-gsgshhhj
-jjsjsjs
-shhdh
+// alert('Hello'); alert('World');
 
-jjjdjl
+//this is not recommended
+// let user = 'John', age = 25, message = 'Hello';
+// alert('Hello heell'); 
+//but this is recommended
 
+// let user = 'John';
+// let age = 25;
+// let message = 'Hello';
 
+let user, name; // can declare two variables at once
+
+name = "Aummal";
+
+admin = name;
+
+alert( admin ); // "John"
